d7be47399331c7f3f021b8fe7912b062aa157986
Sagastein <ishimwe.sage.3@gmail.com>
2023-08-24 23:25:53 +0200

    adding Banner or hero section

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,41 @@
+import SocialMedial from "./SocialMedial";
+
+function Banner() {
+  return (
+    <main className="border  min-h-screen">
+      <aside className="sm:flex">
+        <div className="">
+          <img className="mx-auto" src="SagaBanner.png" alt="sagabanner" />
+        </div>
+        <section className="my-12 flex flex-col">
+          <div className="flex pl-12 gap-x-4 items-center border">
+            <span className="w-16 h-1 block bg-mainblue"></span>
+            <div className="flex text-mainblue font-medium flex-col">
+              <span>Software</span>
+              <span>Developer, Designer</span>
+            </div>
+          </div>
+          <article className="pl-12">
+            <p className=" text-4xl font-Inter md:text-7xl">Hello, I`m</p>
+            <p className="text-4xl md:text-7xl">Ishimwe Sage</p>
+            <p className="my-2 max-w-md font-medium text-darkgrey">
+              {`Hello, I'm passionate about technology, AI, Penetration Testing, and
+            freelancing my skills for real-world impact`}
+            </p>
+            <div className="flex md:mt-12 gap-x-6">
+              <button className="bg-slate-900 hover:bg-transparent hover:text-slate-900 hover:border hover:border-slate-900 text-xs px-4 py-3 rounded text-white font-medium">
+                Get In Touch
+              </button>
+              <button className="text-xs border hover:bg-slate-900 hover:text-white px-4 py-2 font-medium border-slate-900 rounded">
+                View My Resume
+              </button>
+            </div>
+          </article>
+        </section>
+      </aside>
+      <SocialMedial />
+    </main>
+  );
+}
+
+export default Banner;
