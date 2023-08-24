34a09059bddf7ef712597ea7b574826bb6071fe9
Sagastein <ishimwe.sage.3@gmail.com>
2023-08-24 23:28:30 +0200

    app js and main js files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import AboutMe from "./Components/AboutMe";
+import Banner from "./Components/Banner";
+import Contact from "./Components/Contact";
+import Experiance from "./Components/Experiance";
+import Foot from "./Components/Foot";
+import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="w-11/12 mx-auto border">
+        <Banner />
+        <AboutMe />
+        <Services />
+        <Experiance />
+        <Contact />
+      </main>
+      <Foot />
+    </>
+  );
+}
+
+export default App;
