9a30a8f10db1c9cacf0b3968b7a37d98d7ae8599
Sagastein <ishimwe.sage.3@gmail.com>
2023-08-25 10:49:11 +0200

    services

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -36,7 +36,7 @@ function Services() {
     },
   ];
   return (
-    <div className="space-y-4 w-full min-h-full">
+    <div id="services" className="space-y-4 w-full min-h-full">
       <motion.h1
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
