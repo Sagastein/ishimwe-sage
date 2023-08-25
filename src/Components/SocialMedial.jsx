a1c9dc4fd110bb5a6d5446ce2b3e9f26daaf1230
Sagastein <ishimwe.sage.3@gmail.com>
2023-08-25 09:35:10 +0200

    re evaluation the social icons

--- src/Components/SocialMedial.jsx
+++ src/Components/SocialMedial.jsx
@@ -4,30 +4,26 @@ import Gmail from "../assets/Gmail.svg";
 import Instragram from "../assets/Instagram.svg";
 import LinkedIn from "../assets/LinkedIn.svg";
 import Twitter from "../assets/Twitter.svg";
-import "../index.css";
+import { Fade } from "react-awesome-reveal";
 function SocialMedial() {
   return (
-    <section className="flex bg-lightgrey items-center rounded-2xl  border-slate-900  gap-x-4 py-2 my-6 px-4">
-      <p className="text-slate-900 capitalize text-sm font-medium">
-        Connect with me:
-      </p>
-      <div className="scroll-container flex-1">
-        <div className="flex w-[200%]  justify-between items-center horizontal-scroll ">
-          <img className="cursor-pointer" src={Github} alt=".." />
-          <img className="cursor-pointer " src={Figma} alt=".." />
-          <img className="cursor-pointer " src={Gmail} alt=".." />
-          <img className="cursor-pointer " src={Twitter} alt=".." />
-          <img className="cursor-pointer " src={LinkedIn} alt=".." />
-          <img className="cursor-pointer " src={Instragram} alt=".." />
-          <img className="cursor-pointer" src={Github} alt=".." />
-          <img className="cursor-pointer " src={Figma} alt=".." />
-          <img className="cursor-pointer " src={Gmail} alt=".." />
-          <img className="cursor-pointer " src={Twitter} alt=".." />
-          <img className="cursor-pointer " src={LinkedIn} alt=".." />
-          <img className="cursor-pointer " src={Instragram} alt=".." />
+    <Fade cascade fraction={1}>
+      <section className="flex bg-lightgrey items-center rounded-2xl  border-slate-900  gap-x-4 py-2 my-6 px-4">
+        <p className="text-slate-900 capitalize text-sm font-medium">
+          Connect with me:
+        </p>
+        <div className="scroll-container flex-1">
+          <div className="flex   justify-between items-center horizontal-scroll ">
+            <img className="cursor-pointer" src={Github} alt=".." />
+            <img className="cursor-pointer " src={Figma} alt=".." />
+            <img className="cursor-pointer " src={Gmail} alt=".." />
+            <img className="cursor-pointer " src={Twitter} alt=".." />
+            <img className="cursor-pointer " src={LinkedIn} alt=".." />
+            <img className="cursor-pointer " src={Instragram} alt=".." />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
 
