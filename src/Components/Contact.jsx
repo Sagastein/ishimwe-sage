74ea22d6606aa84a69c4f998983ba038c496e4fc
Sagastein <ishimwe.sage.3@gmail.com>
2023-09-08 10:00:40 +0200

    validation with contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,54 @@
 import { motion } from "framer-motion";
 import { MdOutlineMail } from "react-icons/md";
 import { BiPhoneCall, BiSend } from "react-icons/bi";
+import { useState, useRef } from "react"; // Import useState and useRef
+
 function Contact() {
+  // State variables for form inputs and errors
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({});
+
+  // Input references
+  const fullNameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validation logic for each input field
+    const newErrors = {};
+    if (!fullName.trim()) {
+      newErrors.fullName = "Full Name is required";
+      fullNameRef.current.focus();
+    }
+    if (!email.trim() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      newErrors.email = "Please enter a valid email address";
+      emailRef.current.focus();
+    }
+    if (!subject.trim()) {
+      newErrors.subject = "Subject is required";
+      subjectRef.current.focus();
+    }
+    if (!message.trim()) {
+      newErrors.message = "Message is required";
+      messageRef.current.focus();
+    }
+
+    // If there are errors, update the state and prevent form submission
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    // If there are no errors, you can proceed with form submission
+    // Add your code here to handle the form submission, e.g., sending data to a server.
+  };
+
   return (
     <div id="contact" className="space-y-4 py-8">
       <motion.h1
@@ -13,6 +60,7 @@ function Contact() {
         Contact
       </motion.h1>
       <section className="grid md:grid-cols-2 gap-x-28">
+        {/* ... (other content) */}
         <aside className=" space-y-4 p-4">
           <h1 className="text-4xl font-Barlow font-medium">
             Connect with Me: Let's Bring Ideas to Life
@@ -48,14 +96,7 @@ function Contact() {
           </section>
         </aside>
         <aside className="space-y-4 p-4">
-          <h1 className="text-2xl font-Barlow font-medium">
-            Ready to Collaborate? Let's Connect
-          </h1>
-          <p className="font-Barlow">
-            Feel free to reach out for any inquiries, project proposals, or just
-            to say hello. Your message is important to me.
-          </p>
-          <form className="space-y-4" action="." method="post">
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <div className="grid gap-4 md:grid-cols-2">
               <input
                 className="border p-2 w-full outline-none text-darkgrey focus:border-2 rounded-sm focus:border-mainblue"
@@ -63,23 +104,39 @@ function Contact() {
                 name="full name"
                 id="fullName"
                 placeholder="Full Name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                ref={fullNameRef}
               />
+              {errors.fullName && (
+                <p className="text-red-500">{errors.fullName}</p>
+              )}
               <input
                 className="border p-2 w-full outline-none text-darkgrey focus:border-2 rounded-sm focus:border-mainblue"
                 type="email"
-                name="full name"
+                name="email"
                 id="email"
                 placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                ref={emailRef}
               />
+              {errors.email && <p className="text-red-500">{errors.email}</p>}
             </div>
             <div>
               <input
                 className="border p-2 w-full outline-none text-darkgrey focus:border-2 rounded-sm focus:border-mainblue"
                 type="text"
                 name="subject"
-                id="email"
+                id="subject"
                 placeholder="Subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                ref={subjectRef}
               />
+              {errors.subject && (
+                <p className="text-red-500">{errors.subject}</p>
+              )}
             </div>
             <div>
               <textarea
@@ -87,7 +144,13 @@ function Contact() {
                 name="message"
                 id="message"
                 placeholder="Message"
-              ></textarea>
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                ref={messageRef}
+              />
+              {errors.message && (
+                <p className="text-red-500">{errors.message}</p>
+              )}
             </div>
             <button className="py-2 border px-4 rounded-md bg-mainblue hover:opacity-80 text-white font-medium">
               Let's Connect <BiSend className="inline mx-2" />{" "}
